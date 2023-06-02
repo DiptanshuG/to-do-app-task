a16bfee75b4361065d6cb7e746b06b2a8cf9316d
Diptanshu bhawsar <Diptanshubhawsar50@gmail.com>
2023-06-02 19:24:16 +0530

    update

--- src/components/AddTodoForm.tsx
+++ src/components/AddTodoForm.tsx
@@ -2,13 +2,18 @@ import React, { useContext, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { AuthContext } from "../context/AuthContext";
 import { Todo } from "../models/Todo";
-import { Form, Button, Row, Col } from "react-bootstrap";
+import { Form, Button, Row, Col, Alert, Toast } from "react-bootstrap";
 
 export const AddTodoForm: React.FC = () => {
   const [title, setTitle] = useState("");
   const { addTodo } = useContext(AuthContext);
+  const [showToast, setShowToast] = useState(false);
 
   const handleAddTodo = () => {
+    if (!title) {
+      setShowToast(true);
+      return;
+    }
     if (title.trim() !== "") {
       const newTodo: Todo = {
         id: uuidv4(),
@@ -22,22 +27,34 @@ export const AddTodoForm: React.FC = () => {
   };
 
   return (
-    <Form>
-      <Row className="align-items-center">
-        <Col sm={9}>
-          <Form.Control
-            type="text"
-            placeholder="I will do this ..."
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-        </Col>
-        <Col sm={3}>
-          <Button onClick={handleAddTodo} variant="primary">
-            Add Todo
-          </Button>
-        </Col>
-      </Row>
-    </Form>
+    <div>
+      <Form>
+        <Row className="align-items-center">
+          <Col sm={9}>
+            <Form.Control
+              type="text"
+              placeholder="I will do this ..."
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </Col>
+          <Col sm={3}>
+            <Button onClick={handleAddTodo} variant="primary">
+              Add Todo
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+
+      {!title && (
+        <Toast
+          show={showToast}
+          onClose={() => setShowToast(false)}
+          className="mt-2"
+        >
+          <Toast.Body>Please enter a note</Toast.Body>
+        </Toast>
+      )}
+    </div>
   );
 };
--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -25,71 +25,73 @@ const LoginPage: React.FC = () => {
   };
 
   return (
-    <div
-      className="d-flex justify-content-center align-items-center vh-100"
-      style={{ background: "#f8f9fa" }}
-    >
-      <Container className="p-2 rounded  d-flex justify-content-center">
+    <div className="login-page">
       <div
-          className="d-flex flex-lg-row flex-column justify-content-around rounded border border-secondary border-1 shadow"
-          style={{ background: "rgb(0 0 0)" }}
-        >
-          <div className="m-5">
-            <img
-              style={{
-                width: "15rem",
-                height: "20rem",
-
-                borderRadius: "10px",
-                backgroundColor: "#e8e8e8",
-              }}
-              src={todoImg}
-              alt="todoImg"
-            />
-          </div>
-          <div className="m-5">
-            {" "}
-            <Form onSubmit={handleLogin}>
-              <h1
+        className="d-flex justify-content-center align-items-center vh-100"
+        style={{ background: "#dfdccc" }}
+      >
+        <Container className="p-2 rounded  d-flex justify-content-center">
+          <div
+            className="d-flex flex-lg-row flex-column justify-content-around rounded border border-secondary border-1 shadow"
+            style={{ background: "rgb(0 0 0)" }}
+          >
+            <div className="m-5">
+              <img
                 style={{
-                  fontFamily: "Georgia, serif",
-                  fontWeight: "bold",
-                  fontSize: "2rem",
+                  width: "15rem",
+                  height: "20rem",
+
+                  borderRadius: "10px",
+                  backgroundColor: "#e8e8e8",
                 }}
-                className="text-center text-white"
-              >
-                Login
-              </h1>
-              <Form.Group controlId="username">
-                <Form.Label>Username</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Enter your username"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                />
-              </Form.Group>
-              <Form.Group className="mb-2" controlId="password">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="Enter your password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </Form.Group>
-              <Button className="w-100" variant="primary" type="submit">
-                Login
-              </Button>
-              {error && (
-                <Alert className="mt-2" variant="danger">
-                  {error}
-                </Alert>
-              )}
-            </Form>
+                src={todoImg}
+                alt="todoImg"
+              />
+            </div>
+            <div className="m-5">
+              {" "}
+              <Form onSubmit={handleLogin}>
+                <h1
+                  style={{
+                    fontFamily: "Georgia, serif",
+                    fontWeight: "bold",
+                    fontSize: "2rem",
+                  }}
+                  className="text-center text-white"
+                >
+                  Login
+                </h1>
+                <Form.Group controlId="username">
+                  <Form.Label>Username</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter your username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
+                </Form.Group>
+                <Form.Group className="mb-2" controlId="password">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </Form.Group>
+                <Button className="w-100" variant="primary" type="submit">
+                  Login
+                </Button>
+                {error && (
+                  <Alert className="mt-2" variant="danger">
+                    {error}
+                  </Alert>
+                )}
+              </Form>
+            </div>
           </div>
-        </div>
-      </Container>
+        </Container>
+      </div>
     </div>
   );
 };
